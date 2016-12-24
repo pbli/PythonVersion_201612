ff9ef2f19037d58d7a16c3a46dad457a6d9ada21
Pengbo Li <pengbolee@gmail.com>
2016-12-23 20:12:26 -0600

    vs

---
+++ radar/radar/radar.py
@@ -0,0 +1,13 @@
+import csv
+import math
+import numpy as np
+import matplotlib.pyplot as pl
+import pandas as pd
+
+x=[1,2,3]
+pl.subplot(2,1,1)
+pl.plot(x)
+pl.subplot(2,1,2)
+pl.plot(x,x)
+pl.show()
+
