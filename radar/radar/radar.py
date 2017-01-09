5f42b3240c110bffc707813d49a4c4ef0b9a2b14
Pengbo Li <pengbolee@gmail.com>
2017-01-08 22:40:53 -0600

    li

--- radar/radar/radar.py
+++ radar/radar/radar.py
@@ -1,29 +1,27 @@
-import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-reader=csv.reader(open("Scenario_crossing_left_to_right_50mph.csv","r"),delimiter=',')
-keys=next(reader)   #key of data
-x=list(reader)      #data
 objN=40
-result=np.array(x).astype('float')#store data in an array as float type
-#print(keys)
+result=np.genfromtxt("Scenario_crossing_left_to_right_50mph.csv",delimiter=',',skip_header=1)
 #sizeA=result.shape  #size of array
 r=result[:,2] #range
 rdot=result[:,3]/3.6 #range rate
 theta=result[:,4] #angle
 t=result[:,6]# time
+
 error=np.zeros(40)
 for i in range(0,40):
-    error[i]=np.mean(np.absolute(np.sqrt(result[:,7+i]*result[:,7+i]+result[:,i+47]*result[:,i+47])-r))
+    error[i]=np.mean(np.absolute(np.sqrt(result[:,7+i]*result[:,7+i]+result[:,i+47]*result[:,i+47])-r))#1D
 print(np.argmin(error)) #object number is 32, it is 33rd object
+
 RadarPx=result[:,7+32]
 RadarPy=result[:,7+32+40]
 RadarVx=result[:,7+32+80]
 RadarVy=result[:,7+32+120]
 RefPx=r*np.cos(-theta*math.pi/180)
 RefPy=r*np.sin(-theta*math.pi/180)
+
 plt.figure()
 plt.plot(RadarPx,RadarPy,color='Red',linewidth=2.0,label='Radar')
 plt.plot(RefPx,RefPy,color='Blue', linewidth=2.0,label='Reference')
